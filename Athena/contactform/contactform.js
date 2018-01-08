d9c5e3051e30884afd90708e7d64e4878a79f51c
Jose Cabrera <josecabrera244@gmail.com>
2018-01-07 20:05:19 -0600

    Athena man
    Forgot to capitalize the name inside the form file

--- Athena/contactform/contactform.js
+++ Athena/contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "athena/contactform/contactform.php",
+      url: "Athena/contactform/contactform.php",
       data: str,
       success: function(msg) {
         // alert(msg);
